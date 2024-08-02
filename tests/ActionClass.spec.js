8a6acac3a71e966cb0637d1f8bf45ea68078f1d4
Vishwas <66787592+Vishwas-web@users.noreply.github.com>
2024-08-02 11:21:26 +0000

    Action Class

--- tests/ActionClass.spec.js
+++ tests/ActionClass.spec.js
@@ -4,4 +4,29 @@ test('Action Class-Text Input',async({page})=>{
     await page.goto('https://www.letskodeit.com/practice')
     await page.locator('id=autosuggest').fill("Hello")
     await page.locator('id=autosuggest').fill("bye")
-})
+});
+
+test('CheckBox Radio Buttons',async({page})=>{
+    await page.goto('https://www.letskodeit.com/practice')
+    //Selecting an CheckBox.
+    await page.locator('id=bmwcheck').click()
+    expect(page.locator('id=bmwcheck')).toBeChecked()
+    //Taking Screenshot
+    await page.screenshot({ path: 'screenshot3.png', fullPage: true })
+    //Selecting an Radio button.
+    await page.locator('id=bmwradio').click();
+    //Taking Screenshot
+    await page.screenshot({ path: 'screenshot2.png', fullPage: true })
+});
+
+test('Select Options',async({page})=>{
+    await page.goto('https://www.letskodeit.com/practice')
+    //Selecting One Value From the DropDown
+    await page.locator('id=carselect').selectOption('bmw')
+    //Taking Screenshot
+    await page.screenshot({ path: 'screenshot.png', fullPage: true })
+    //Selecting Mutiple Records
+    await page.locator('id=multiple-select-example').selectOption(['apple'],['orange'],['peach'])
+    //Taking Screenshot
+    await page.screenshot({ path: 'screenshot1.png', fullPage: true })
+});
